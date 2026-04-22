75b969682ec26544eb174565bf1bff781edb8057
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-22 14:27:32 +0200

    Implement basic API data fetching

--- src/ingestion/ruter_api_ingest.py
+++ src/ingestion/ruter_api_ingest.py
@@ -1,8 +1,18 @@
 import requests
 import json
 
+
 def fetch_data():
-    print("Fetching transport data...")
+    url = "https://jsonplaceholder.typicode.com/posts/1"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(json.dumps(data, indent=4))
+    else:
+        print("Failed to fetch data")
+
 
 if __name__ == "__main__":
     fetch_data()
