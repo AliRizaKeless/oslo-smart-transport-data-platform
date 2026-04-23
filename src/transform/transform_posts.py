9bb3ca20401e524ba5746e3f768be4323b28abde
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-23 14:48:39 +0200

    Export processed data to CSV format

--- src/transform/transform_posts.py
+++ src/transform/transform_posts.py
@@ -1,9 +1,11 @@
 import json
+import csv
 
 
 def transform_data():
     input_path = "data/raw/sample_response.json"
-    output_path = "data/processed/processed_post.json"
+    output_json_path = "data/processed/processed_post.json"
+    output_csv_path = "data/processed/processed_post.csv"
 
     with open(input_path, "r", encoding="utf-8") as file:
         data = json.load(file)
@@ -14,10 +16,16 @@ def transform_data():
         "body": data["body"]
     }
 
-    with open(output_path, "w", encoding="utf-8") as file:
+    with open(output_json_path, "w", encoding="utf-8") as file:
         json.dump(transformed_data, file, indent=4)
 
-    print(f"Transformed data saved to {output_path}")
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=transformed_data.keys())
+        writer.writeheader()
+        writer.writerow(transformed_data)
+
+    print(f"Transformed data saved to {output_json_path}")
+    print(f"CSV data saved to {output_csv_path}")
 
 
 if __name__ == "__main__":
