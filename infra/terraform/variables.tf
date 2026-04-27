db136a3f6221ac111417ed0018c8de913f14fad5
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-27 21:38:47 +0200

    Add Terraform variables for Azure resource group

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "rg-oslo-transport-data-dev"
+}
+
+variable "location" {
+  description = "Azure region for the resources"
+  type        = string
+  default     = "norwayeast"
+}
