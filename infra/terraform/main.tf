f260e05ecf85af4292b07066f9bb5902837ac9ce
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-28 22:46:47 +0200

    Add Azure Storage Account configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_blob_public_access = false
+}
