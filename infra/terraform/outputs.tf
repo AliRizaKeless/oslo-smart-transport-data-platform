87fd00e6d1f93bc38b6fffca55b8870e3ded582b
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-28 22:58:02 +0200

    Add Terraform outputs for storage resources

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -7,3 +7,13 @@ output "resource_group_location" {
   description = "Location of the created Azure Resource Group"
   value       = azurerm_resource_group.main.location
 }
+
+output "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  value       = azurerm_storage_account.main.name
+}
+
+output "raw_container_name" {
+  description = "Name of the raw data container"
+  value       = azurerm_storage_container.raw.name
+}
