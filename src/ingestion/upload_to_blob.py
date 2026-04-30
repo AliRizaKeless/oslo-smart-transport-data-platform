1ff797f348bdda866cf8fd7f11a20e0b74a37a2d
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-30 14:00:27 +0200

    Implement Azure Blob upload logic

--- src/ingestion/upload_to_blob.py
+++ src/ingestion/upload_to_blob.py
@@ -4,7 +4,26 @@ from azure.storage.blob import BlobServiceClient
 
 
 def upload_file_to_blob():
-    print("Azure Blob upload step started...")
+    load_dotenv()
+
+    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    container_name = os.getenv("AZURE_STORAGE_RAW_CONTAINER")
+    local_file_path = "data/raw/sample_response.json"
+    blob_name = "sample_response.json"
+
+    if not connection_string:
+        raise ValueError("AZURE_STORAGE_CONNECTION_STRING is missing")
+
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name,
+        blob=blob_name
+    )
+
+    with open(local_file_path, "rb") as file:
+        blob_client.upload_blob(file, overwrite=True)
+
+    print(f"Uploaded {local_file_path} to container '{container_name}'")
 
 
 if __name__ == "__main__":
