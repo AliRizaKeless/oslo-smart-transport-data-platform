99b35d6baa0e52bc841ae618fc87d04fad1ab10c
Ali Riza Keles <Alirizakeles66@gmail.com>
2026-04-30 13:50:34 +0200

    Add initial Azure Blob upload script

---
+++ src/ingestion/upload_to_blob.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient
+
+
+def upload_file_to_blob():
+    print("Azure Blob upload step started...")
+
+
+if __name__ == "__main__":
+    upload_file_to_blob()
